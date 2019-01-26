b7f0a95b66500e95302f4c5fa8791227d0092b35
Siarhei Dziki <siarheidziki@ray-games.com>
2019-01-26 14:51:31 +0300

    - removed cpp source code and replaced with blueprint only project

--- Source/curtains_cat/curtains_cat.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "curtains_cat.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, curtains_cat, "curtains_cat" );
